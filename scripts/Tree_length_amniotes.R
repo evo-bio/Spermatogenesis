30f6ee56945f2ad193350ca696da9961f49a7ab1
evo-bio <92106255+evo-bio@users.noreply.github.com>
2022-11-04 16:24:31 +0100

    Update Tree_length_amniotes.R

--- scripts/Tree_length_amniotes.R
+++ scripts/Tree_length_amniotes.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(cowplot)
 library(ape)
 
-# Loading gene expression data per cell type biological replicate across 1-1 orthologs
+# Loading gene expression data per cell type and biological replicate across 1-1 orthologs
 mat <- readRDS( file="data/Tree.length.amniotes.input.rds" )
 
 tissues <- c("Other_somatic",
